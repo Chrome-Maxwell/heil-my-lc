e41a998a2f3492a7f75ea1993dd7aca2a19b4210
hilbert_workstation <chenyuang@sjtu.edu.cn>
2021-09-29 13:36:49 -0400

    feat: problem no.283.

---
+++ src/283.move-zeroes.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=283 lang=cpp
+ *
+ * [283] Move Zeroes
+ */
+#include <bits/stdc++.h>
+using namespace std;
+// @lc code=start
+class Solution
+{
+public:
+    void moveZeroes(vector<int> &nums)
+    {
+        
+        vector<int>::iterator l = nums.end(), r;
+        for (auto i = nums.begin(); i != nums.end(); ++i)
+        {
+            if (*i == 0)
+            {
+                l = i;
+                break;
+            }
+        }
+        if(l==nums.end()) return;
+        for (r = l + 1; r != nums.end(); ++r)
+        {
+            if (*r != 0)
+            {
+                swap(*l, *r);
+                l++;
+            }
+        }
+    }
+};
+// @lc code=end

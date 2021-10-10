f0cdb66f568ef2c715d6b329c7fdb1c16ac1725f
hilbert_workstation <chenyuang@sjtu.edu.cn>
2021-10-09 23:07:07 -0400

    feat: problem no.77.

---
+++ src/77.combinations.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=77 lang=cpp
+ *
+ * [77] Combinations
+ */
+#include <bits/stdc++.h>
+using namespace std;
+// @lc code=start
+class Solution {
+private:
+    vector<vector<int>> result;
+    vector<int> path;
+    void backtrace(int n, int k, int cur) {
+        int size = path.size();
+        if(size == k) {
+            result.push_back(path);
+            return;
+        }
+        for(int i = cur; i <= n - (k - size) + 1; i++) {
+            path.push_back(i);
+            backtrace(n, k, i + 1);
+            path.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> combine(int n, int k) {
+        backtrace(n, k, 1);
+        return result;
+    }
+};
+// @lc code=end

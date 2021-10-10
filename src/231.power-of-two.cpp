e271070d8b0d0b1ff0bb91762b6da137313897a1
hilbert_workstation <chenyuang@sjtu.edu.cn>
2021-10-09 23:52:25 -0400

    feat: special solution of problem no.231.

--- src/231.power-of-two.cpp
+++ src/231.power-of-two.cpp
@@ -5,12 +5,19 @@
  */
 
 // @lc code=start
-class Solution {
+// class Solution {
+// public:
+//     bool isPowerOfTwo(int n) {
+//         if(n <= 0) return false;
+//         return n==1 ? true :  (!(n & 1)) && isPowerOfTwo(n >> 1);
+//     }
+// };
+class Solution
+{
 public:
-    bool isPowerOfTwo(int n) {
-        if(n <= 0) return false;
-        return n==1 ? true :  (!(n & 1)) && isPowerOfTwo(n >> 1);
+    bool isPowerOfTwo(int n)
+    {
+        return n > 0 && ((n & n - 1) == 0); // that's cool...
     }
 };
 // @lc code=end
-

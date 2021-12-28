b27ca78e33e5eee34f7b303a8c1760632c21b352
hilbert_workstation <chenyuang@sjtu.edu.cn>
2021-12-28 16:01:39 -0500

    feat: problem 1995 dp method

--- src/1995.count-special-quadruplets.cpp
+++ src/1995.count-special-quadruplets.cpp
@@ -6,33 +6,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 // @lc code=start
+// class Solution {
+// public:
+//   int countQuadruplets(vector<int> &nums) {
+//     n = nums.size();
+//     cnt = 0;
+//     backtrace(nums, 0);
+//     return cnt;
+//   }
+
+// private:
+//   int n;
+//   int cnt;
+//   vector<int> path;
+//   void backtrace(vector<int> &nums, int beg) {
+//     if (path.size() == 3) {
+//       int sum = nums[path[0]] + nums[path[1]] + nums[path[2]];
+//       for (; beg < n; beg++) {
+//         if (sum == nums[beg])
+//           cnt++;
+//       }
+//       return;
+//     }
+//     for (int i = beg; i < n; i++) {
+//       path.push_back(i);
+//       backtrace(nums, i + 1);
+//       path.pop_back();
+//     }
+//   }
+// };
+#define rep(i, a, b) for (int i = (a); i <= (b); ++i)
+#define re_(i, a, b) for (int i = (a); i < (b); ++i)
+#define dwn(i, a, b) for (int i = (a); i >= (b); --i)
 class Solution {
 public:
   int countQuadruplets(vector<int> &nums) {
-    n = nums.size();
-    cnt = 0;
-    backtrace(nums, 0);
-    return cnt;
-  }
-
-private:
-  int n;
-  int cnt;
-  vector<int> path;
-  void backtrace(vector<int> &nums, int beg) {
-    if (path.size() == 3) {
-      int sum = nums[path[0]] + nums[path[1]] + nums[path[2]];
-      for (; beg < n; beg++) {
-        if (sum == nums[beg])
-          cnt++;
-      }
-      return;
+    int n = nums.size(), ans = 0, mx = *max_element(nums.begin(), nums.end());
+    vector<vector<int>> dp(mx + 1, vector<int>(4));
+    dp[0][0] = 1;
+    rep(i, 1, n) {
+      int v = nums[i - 1];
+      ans += dp[v][3];
+      dwn(j1, mx, v) { dwn(j2, 3, 1) dp[j1][j2] += dp[j1 - v][j2 - 1]; }
     }
-    for (int i = beg; i < n; i++) {
-      path.push_back(i);
-      backtrace(nums, i + 1);
-      path.pop_back();
-    }
+    return ans;
   }
 };
 // [28,8,49,85,37,90,20,8] -> 1
